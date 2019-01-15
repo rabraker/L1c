6ea117b9ae633cad45a33421a4355bedf710f3a0
arnold (zeus) <abraker@fastmail.com>
2019-01-15 01:38:51 -0700

    Get a bit more performance of dct_mkl by using some cblas_ and mkl v? functions. In some places, it seems faster to do it on our own.

--- src/dct_mkl.c
+++ src/dct_mkl.c
@@ -69,10 +69,8 @@ void dctmkl_idct(double * restrict x, double * restrict y){
   double *x_ = __builtin_assume_aligned(x, 64);
   double *y_ = __builtin_assume_aligned(y, 64);
 
-  for (l1c_int i=0; i<dctmkl_Nx; i++){
-      y_[i] = x_[i];
-    }
-  // cblas_dcopy(dctmkl_Nx, x, 1, y, 1);
+  cblas_dcopy(dctmkl_Nx, x_, 1, y_, 1);
+
   dctmkl_ipar[7] = 0;
   dctmkl_ipar[10] = 1;
 
@@ -83,7 +81,7 @@ void dctmkl_idct(double * restrict x, double * restrict y){
 void dctmkl_EMx_new(double * restrict x, double * restrict y){
   /* Compute y = E * M *dctmkl_x, where M is the IDCT, and E is the subsampling matrix.
 
-     --x_fftw should have size dctmkl_Nx.
+     -- x should have size dctmkl_Nx.
      -- y should have size at least dctmkl_Ny.
      On exit, the first dctmkl_Ny entries of y will contain the result of E * M *x.
   */
@@ -94,26 +92,20 @@ void dctmkl_EMx_new(double * restrict x, double * restrict y){
   // l1c_int idx_ = __builtin_assume_aligned(dctmkl_pix_mask_idx, 64);
 
   l1c_int i=0;
-  // Leave the input vector unchanged.
-  double x0_tmp = x_a[0];
-  x_a[0] = x_a[0] * sqrt(2.0);
-
-  for (i=0; i<dctmkl_Nx; i++){
-    tmp_x[i] = x_a[i];
-  }
-  tmp_x[dctmkl_Nx] = 0.0; //Strange mkl requirement. dctmkl_tmp_x should have size N+1
 
 
-  dctmkl_ipar[10] = 1; //Dont normalize
+  cblas_dcopy(dctmkl_Nx, x_a, 1, tmp_x, 1);
+  tmp_x[0] = tmp_x[0] * sqrt(2.0);  //proper scaling, so transforms are transposes.
+  tmp_x[dctmkl_Nx] = 0.0;           //Strange mkl requirement. dctmkl_tmp_x should have size N+1
+
+
+  dctmkl_ipar[10] = 1; //Dont normalize the result, because the do it wrong.
   d_forward_trig_transform(tmp_x, &mkl_dct_handle, dctmkl_ipar, dctmkl_dpar, &dctmkl_stat);
 
-  // Apply the subsampling operation.
+  //Apply the subsampling operation.
   for (i=0; i<dctmkl_Ny; i++){
     y_a[i] = tmp_x[ dctmkl_pix_mask_idx[i]] * dctmkl_root_1_by_2N;
   }
-
-  /*Undo the scaling operation. */
-  x_a[0] = x0_tmp;
 
 }
 
@@ -131,22 +123,21 @@ void dctmkl_MtEty( double * restrict y, double * restrict x){
 
   // E^T * y --> y_sparse. y_sparse should be set to all zeros, and pix_mask does not change.
   // Returns a pointer to the array containing the result, which has length N.
+
   for(i=0; i<dctmkl_Nx+1; i++){
-   tmp_x_a[i] = 0.0;
+    tmp_x_a[i] = 0.0;
   }
 
-  for (i=0; i<dctmkl_Ny; i++){
-    tmp_x_a[dctmkl_pix_mask_idx[i]] = y_a[i];
-  }
+  // I.e., tmp_x_a[pix_idx] = y_a;
+  vdUnpackV(dctmkl_Ny, y_a, tmp_x_a, dctmkl_pix_mask_idx);
 
-  // fftw_execute(dctmkl_plan_MtEty); // This is the M^T * dctmkl_y_sparse
+
   dctmkl_ipar[10] = 1; //Dont normalize
   d_backward_trig_transform(tmp_x_a, &mkl_dct_handle, dctmkl_ipar, dctmkl_dpar, &dctmkl_stat);
 
   tmp_x_a[0] = tmp_x_a[0]/sqrt(2.0);
-  for (i=0; i<dctmkl_Nx; i++){
-    x_a[i] = tmp_x_a[i] * dctmkl_root_1_by_2N;;
-  }
+
+  cblas_daxpby(dctmkl_Nx, dctmkl_root_1_by_2N, tmp_x_a, 1, 0.0, x_a, 1);
 }
 
 
