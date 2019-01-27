35627fcc2c82e9b673d9049997f03f40a340c8e7
arnold (zeus) <abraker@fastmail.com>
2019-01-27 01:45:28 -0700

    Initial working implementation of using autotools to build. Not very good yet, too much is hard coded.

---
+++ clean_autotools.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+rm -f ./aclocal.m4
+rm -f -r autom4te.cache
+rm -f config.h.in
+rm -f config.status
+rm -f configure
+rm -f install-sh autoscan.log config.h ltmain.sh
+rm -f missing libtool stamp-h1 config.log
+rm -f Makefile.in ar-lib compile config.guess config.sub depcomp
