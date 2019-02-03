f3b53b84e920bc52164d6c83db766c7fc63f19d4
arnold (zeus) <abraker@fastmail.com>
2019-02-02 22:59:37 -0700

    Streamline configure.ac to collect cpp and cflags for the different compilations (e.g., mex files, libl1c source, unit tests etc. Add checks for _mm_malloc and posix_memalign.

---
+++ config-fftw-ob.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export CPPFLAGS="-I/usr/local/include -I/usr/local/MATLAB/R2018b/extern/include"
+export LDFLAGS="-L/usr/local/lib -L/usr/local/MATLAB/R2018b/bin/glnxa64"
+
+./configure --with-fftw3 --with-mex
+# --with-mex
+# ./configure $@
+
---
+++ config-mkl-mex.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export CPPFLAGS="-I/opt/intel/mkl/include -I/usr/local/MATLAB/R2018b/extern/include"
+export LDFLAGS="-L/opt/intel/mkl/lib/intel64 -L/usr/local/MATLAB/R2018b/bin/glnxa64"
+
+./configure --with-mkl
+# --with-mex
+# ./configure $@
+
