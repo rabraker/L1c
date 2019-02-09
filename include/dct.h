4b30a30c226de0029c0bdb9581776642c98b667f
arnold (zeus) <abraker@fastmail.com>
2019-02-09 10:59:59 -0700

    No return type.

--- include/dct.h
+++ include/dct.h
@@ -7,6 +7,6 @@ extern void dct_destroy();
 
 extern void dct_EMx_new(double *x_fftw, double *y);
 extern void dct_MtEty(double *y, double *x);
-extern double* dct_idct(double *x_fftw);
+extern void dct_idct(double *x_fftw);
 extern void dct_MtEt_EMx_new(double *x_fftw, double *z);
 #endif
