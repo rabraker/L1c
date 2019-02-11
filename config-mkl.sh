a93efd1e7b04b95882a407ce3aa487546bc1e3b3
arnold (zeus) <abraker@fastmail.com>
2019-02-11 12:37:25 -0700

    Tweak config drivers.

--- config-mkl.sh
+++ config-mkl.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export CPPFLAGS="-I/opt/intel/mkl/include -I/usr/local/R2018b/extern/include" 
-export LDFLAGS="-L/opt/intel/mkl/lib/intel64 -L/usr/local/MATLAB/R2018b/bin/glnxa64" 
+export CPPFLAGS="-I/opt/intel/mkl/include"
+export LDFLAGS="-L/opt/intel/mkl/lib/intel64"
 
-./configure $@
+./configure --with-mkl $@
 
 
