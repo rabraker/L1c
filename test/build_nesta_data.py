a297cf38d7d289b0308d847c402f4d793365088e
arnold <abraker@fastmail.com>
2019-05-26 18:11:32 -0600

    Build nesta test data automatically.

--- test/build_nesta_data.py
+++ test/build_nesta_data.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+
+import os
 import numpy as np
-import numpy.testing as npt
+# import numpy.testing as npt
 import L1cTestDataUtils as TDU
 from scipy.fftpack import dct
-import ipdb
+
 
 def Adct_factory(pix_idx):
     def Adct(x):
@@ -41,14 +43,14 @@ def Utdct_factory():
 def nesta_project(x, g, b, L_mu, A, At, epsilon):
     """
     Implements the closed form solution to
-    yk = argmin_{x\in Q_p} (Lmu/2) || xk - x||_2^2 + <\Nablaf_mu(xk), x-xk> (3.3)
+
+    yk = argmin_{x\\in Q_p} (Lmu/2) || xk - x||_2^2 + <\\Nabla f_mu(xk), x-xk> (3.3)
     where Q_p = ||b - Ayk||_2^2 <= \epsilon
 
     i.e., eq (3.5) and (3.6) and (3.7)
 
     Note that because A is an othogonal projector, then AA'=I (but not A'A).
     Thus, in (3.6), A'AA'b = A'b.
-
     """
 
     q = x - (1.0 / L_mu) * g
@@ -76,60 +78,67 @@ def nesta_f_eval(U, Ut, xk, mu):
 
     return fx, gradf
 
+if __name__ == "__main__":
 
-pix_idx = np.array([0, 3, 4, 7, 8])
-M = 10
-N = len(pix_idx)
+    srcdir = os.getenv("srcdir")
+    if srcdir is None:
+        srcdir = "."
 
-L = 1.0
-sigma = 0.1
-mu = 0.1
-Lmu = L / mu
+    data_dir = srcdir+"/test_data"
 
-xk = np.random.randn(M)
-b = np.random.randn(N)
-g = np.random.randn(M)
+    pix_idx = np.array([0, 3, 4, 7, 8])
+    M = 10
+    N = len(pix_idx)
 
-q = xk - (1.0/Lmu)*g
+    L = 1.0
+    sigma = 0.1
+    mu = 0.1
+    Lmu = L / mu
 
-A = Adct_factory(pix_idx)
-At = Atdct_factory(pix_idx, M)
-U = Udct_factory()
-Ut = Utdct_factory()
+    xk = np.random.randn(M)
+    b = np.random.randn(N)
+    g = np.random.randn(M)
 
-Atb = At(b)
+    q = xk - (1.0/Lmu)*g
+
+    A = Adct_factory(pix_idx)
+    At = Atdct_factory(pix_idx, M)
+    U = Udct_factory()
+    Ut = Utdct_factory()
+
+    Atb = At(b)
 
 
-yk, lam = nesta_project(xk, g, b, Lmu, A, At,  sigma)
+    yk, lam = nesta_project(xk, g, b, Lmu, A, At,  sigma)
 
-fx, gradf = nesta_f_eval(U, Ut, xk, mu)
+    fx, gradf = nesta_f_eval(U, Ut, xk, mu)
 
-# I = np.eye(N)
-lhs = yk + (lam/Lmu)*At(A(yk))
-rhs = (lam/Lmu)*Atb + xk - (1.0/Lmu)*g
+    # I = np.eye(N)
+    lhs = yk + (lam/Lmu)*At(A(yk))
+    rhs = (lam/Lmu)*Atb + xk - (1.0/Lmu)*g
 
-print(np.linalg.norm(b - A(yk)) - sigma < 0)
-print(np.linalg.norm(b - A(yk)) - sigma)
+    print(np.linalg.norm(b - A(yk)) - sigma < 0)
+    print(np.linalg.norm(b - A(yk)) - sigma)
 
-# npt.assert_array_almost_equal(lhs, rhs)
-print(lhs-rhs)
+    # npt.assert_array_almost_equal(lhs, rhs)
+    print(lhs-rhs)
 
-q = At(A(q))
-dat = {'N': N,
-       'xk': xk,
-       'b': b,
-       'g': g,
-       'q': q,
-       'gradf_exp': gradf,
-       'fx_exp': fx,
-       'yk_exp': yk,
-       'pix_idx': pix_idx,
-       'Lmu': Lmu,
-       'L': L,
-       'mu': mu,
-       'sigma': sigma,
-       }
+    q = At(A(q))
+    dat = {'N': N,
+           'xk': xk,
+           'b': b,
+           'g': g,
+           'q': q,
+           'gradf_exp': gradf,
+           'fx_exp': fx,
+           'yk_exp': yk,
+           'pix_idx': pix_idx,
+           'Lmu': Lmu,
+           'L': L,
+           'mu': mu,
+           'sigma': sigma,
+           }
 
-dat = TDU.jsonify(dat)
+    dat = TDU.jsonify(dat)
 
-TDU.save_json(dat, 'test_data/nesta_data.json')
+    TDU.save_json(dat, data_dir+"/nesta_data.json")
