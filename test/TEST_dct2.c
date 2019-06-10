5c0ca79e7760da12cbe1a1ed4bfe03994e6c6765
arnold <abraker@fastmail.com>
2019-06-10 00:04:37 -0600

    Simplify dct2 test setup and teardown.

--- test/TEST_dct2.c
+++ test/TEST_dct2.c
@@ -56,7 +56,7 @@ typedef struct DctData{
   l1c_int n;
 
   int setup_status;
-  char fpath[256];
+  char *fpath;
 
 }DctData;
 
@@ -70,143 +70,109 @@ l1c_AxFuns ax_funs;
    the associated setup and teardown functions for each test case. This allows us
    to use a different file path for each.
  */
-static void setup(DctData *dct_dat){
+static void setup(char *fname){
   cJSON *test_data_json;
-
   int setup_status=0;
-  if (load_file_to_json(dct_dat->fpath, &test_data_json)){
+
+  dctd = malloc(sizeof(DctData));
+  if (!dctd){
+    fprintf(stderr, "Memory allocation failed in %s\n", __func__);
+    ck_abort();
+  }
+
+  dctd->fpath  = fullfile(test_data_dir, fname);
+  if (!dctd->fpath){
+    fprintf(stderr, "Memory allocation failed in %s\n", __func__);
+    ck_abort();
+  }
+  if (load_file_to_json(dctd->fpath, &test_data_json)){
     fprintf(stderr, "Error loading data in test_dct\n");
     ck_abort();
   }
 
-  setup_status +=extract_json_double_array(test_data_json, "x_in", &dct_dat->x_in, &dct_dat->mtot);
-  setup_status +=extract_json_double_array(test_data_json, "y_in", &dct_dat->y_in, &dct_dat->n);
-  setup_status +=extract_json_double_array(test_data_json, "z_in", &dct_dat->z_in, &dct_dat->mtot);
+  setup_status +=extract_json_double_array(test_data_json, "x_in", &dctd->x_in, &dctd->mtot);
+  setup_status +=extract_json_double_array(test_data_json, "y_in", &dctd->y_in, &dctd->n);
+  setup_status +=extract_json_double_array(test_data_json, "z_in", &dctd->z_in, &dctd->mtot);
 
-  setup_status +=extract_json_double_array(test_data_json, "MtEt_EMx", &dct_dat->MtEty_EMx_exp, &dct_dat->mtot);
+  setup_status +=extract_json_double_array(test_data_json, "MtEt_EMx", &dctd->MtEty_EMx_exp, &dctd->mtot);
 
-  setup_status +=extract_json_double_array(test_data_json, "MtEty", &dct_dat->MtEty_exp, &dct_dat->mtot);
+  setup_status +=extract_json_double_array(test_data_json, "MtEty", &dctd->MtEty_exp, &dctd->mtot);
 
-  setup_status +=extract_json_double_array(test_data_json, "EMx", &dct_dat->EMx_exp, &dct_dat->n);
+  setup_status +=extract_json_double_array(test_data_json, "EMx", &dctd->EMx_exp, &dctd->n);
 
-  setup_status +=extract_json_double_array(test_data_json, "Mx", &dct_dat->Mx_exp, &dct_dat->mtot);
-  setup_status +=extract_json_double_array(test_data_json, "Mty", &dct_dat->Mty_exp, &dct_dat->mtot);
-  setup_status +=extract_json_double_array(test_data_json, "Ex", &dct_dat->Ex_exp, &dct_dat->n);
-  setup_status +=extract_json_double_array(test_data_json, "Ety", &dct_dat->Ety_exp, &dct_dat->mtot);
+  setup_status +=extract_json_double_array(test_data_json, "Mx", &dctd->Mx_exp, &dctd->mtot);
+  setup_status +=extract_json_double_array(test_data_json, "Mty", &dctd->Mty_exp, &dctd->mtot);
+  setup_status +=extract_json_double_array(test_data_json, "Ex", &dctd->Ex_exp, &dctd->n);
+  setup_status +=extract_json_double_array(test_data_json, "Ety", &dctd->Ety_exp, &dctd->mtot);
 
-  setup_status +=extract_json_int_array(test_data_json, "pix_idx", &dct_dat->pix_idx, &dct_dat->n);
-  setup_status +=extract_json_int(test_data_json, "mrow", &dct_dat->mrow);
-  setup_status +=extract_json_int(test_data_json, "mcol", &dct_dat->mcol);
+  setup_status +=extract_json_int_array(test_data_json, "pix_idx", &dctd->pix_idx, &dctd->n);
+  setup_status +=extract_json_int(test_data_json, "mrow", &dctd->mrow);
+  setup_status +=extract_json_int(test_data_json, "mcol", &dctd->mcol);
 
   if (setup_status){
     fprintf(stderr, "Error Loading json into program data in 'test_MtEty_large()'. Aborting\n");
     ck_abort();
   }
-  ck_assert_int_eq(dct_dat->mrow * dct_dat->mcol, dct_dat->mtot);
+  ck_assert_int_eq(dctd->mrow * dctd->mcol, dctd->mtot);
 
-  dct_dat->MtEty_EMx_act = l1c_malloc_double(dct_dat->mtot);
-  dct_dat->MtEty_act = l1c_malloc_double(dct_dat->mtot);
-  dct_dat->EMx_act = l1c_malloc_double(dct_dat->mtot);
-  dct_dat->Mx_act = l1c_malloc_double(dct_dat->mtot);
-  dct_dat->Mty_act = l1c_malloc_double(dct_dat->mtot);
-  dct_dat->Ex_act = l1c_calloc_double(dct_dat->n);
-  dct_dat->Ety_act = l1c_malloc_double(dct_dat->mtot);
+  dctd->MtEty_EMx_act = l1c_malloc_double(dctd->mtot);
+  dctd->MtEty_act = l1c_malloc_double(dctd->mtot);
+  dctd->EMx_act = l1c_malloc_double(dctd->mtot);
+  dctd->Mx_act = l1c_malloc_double(dctd->mtot);
+  dctd->Mty_act = l1c_malloc_double(dctd->mtot);
+  dctd->Ex_act = l1c_calloc_double(dctd->n);
+  dctd->Ety_act = l1c_malloc_double(dctd->mtot);
 
-  l1c_dct2_setup( dct_dat->n, dct_dat->mrow, dct_dat->mcol, dct_dat->pix_idx, &ax_funs);
+  l1c_dct2_setup( dctd->n, dctd->mrow, dctd->mcol, dctd->pix_idx, &ax_funs);
 
   cJSON_Delete(test_data_json);
 }
 
-static void teardown(DctData *dct_dat){
-  l1c_free_double(dct_dat->x_in);
-  l1c_free_double(dct_dat->y_in);
-  l1c_free_double(dct_dat->z_in);
+static void teardown(void){
+  l1c_free_double(dctd->x_in);
+  l1c_free_double(dctd->y_in);
+  l1c_free_double(dctd->z_in);
 
-  l1c_free_double(dct_dat->MtEty_EMx_exp);
-  l1c_free_double(dct_dat->MtEty_exp);
-  l1c_free_double(dct_dat->EMx_exp);
-  l1c_free_double(dct_dat->Mx_exp);
-  l1c_free_double(dct_dat->Mty_exp);
-  l1c_free_double(dct_dat->Ex_exp);
-  l1c_free_double(dct_dat->Ety_exp);
+  l1c_free_double(dctd->MtEty_EMx_exp);
+  l1c_free_double(dctd->MtEty_exp);
+  l1c_free_double(dctd->EMx_exp);
+  l1c_free_double(dctd->Mx_exp);
+  l1c_free_double(dctd->Mty_exp);
+  l1c_free_double(dctd->Ex_exp);
+  l1c_free_double(dctd->Ety_exp);
 
-  l1c_free_double(dct_dat->MtEty_EMx_act);
-  l1c_free_double(dct_dat->MtEty_act);
-  l1c_free_double(dct_dat->EMx_act);
-  l1c_free_double(dct_dat->Mx_act);
-  l1c_free_double(dct_dat->Mty_act);
-  l1c_free_double(dct_dat->Ex_act);
-  l1c_free_double(dct_dat->Ety_act);
+  l1c_free_double(dctd->MtEty_EMx_act);
+  l1c_free_double(dctd->MtEty_act);
+  l1c_free_double(dctd->EMx_act);
+  l1c_free_double(dctd->Mx_act);
+  l1c_free_double(dctd->Mty_act);
+  l1c_free_double(dctd->Ex_act);
+  l1c_free_double(dctd->Ety_act);
 
-  free(dct_dat->pix_idx);
+  free(dctd->pix_idx);
   ax_funs.destroy();
+  free(dctd->fpath);
+
+  free(dctd);
 
 }
 
 static void setup_square(void){
-  dctd = malloc(sizeof(DctData));
-
-  char *fpath_dct2_small = fullfile(test_data_dir, "dct2_small.json");
-
-  sprintf(dctd->fpath, "%s", fpath_dct2_small);
-  setup(dctd);
-
-  free(fpath_dct2_small);
-}
-
-static void teardown_square(void){
-  teardown(dctd);
-  free(dctd);
+  setup("dct2_small.json");
 }
 
 static void setup_tall(void){
-  dctd = malloc(sizeof(DctData));
-
-  char *fpath_dct2_small = fullfile(test_data_dir, "dct2_small_tall.json");
-
-  sprintf(dctd->fpath, "%s", fpath_dct2_small);
-  setup(dctd);
-
-  free(fpath_dct2_small);
-}
-
-static void teardown_tall(void){
-  teardown(dctd);
-  free(dctd);
+  setup("dct2_small_tall.json");
 }
 
 
 static void setup_wide(void){
-  dctd = malloc(sizeof(DctData));
-
-  char *fpath_dct2_small = fullfile(test_data_dir, "dct2_small_wide.json");
-
-  sprintf(dctd->fpath, "%s", fpath_dct2_small);
-  setup(dctd);
-
-  free(fpath_dct2_small);
-}
-
-static void teardown_wide(void){
-  teardown(dctd);
-  free(dctd);
+  setup( "dct2_small_wide.json");
 }
 
 
 static void setup_pure_square(void){
-  dctd = malloc(sizeof(DctData));
-
-  char *fpath_dct2_pure = fullfile(test_data_dir, "dct2_small_pure_dct.json");
-  sprintf(dctd->fpath, "%s", fpath_dct2_pure);
-  setup(dctd);
-
-  free(fpath_dct2_pure);
-}
-
-static void teardown_pure_square(void){
-  teardown(dctd);
-  free(dctd);
-
+  setup("dct2_small_pure_dct.json");
 }
 
 
@@ -294,7 +260,7 @@ Suite *dct2_suite(void)
 
 
   tc_dct2_square = tcase_create("dct2_small");
-  tcase_add_checked_fixture(tc_dct2_square, setup_square, teardown_square);
+  tcase_add_checked_fixture(tc_dct2_square, setup_square, teardown);
   tcase_add_test(tc_dct2_square, test_dct2_MtEt_EMx);
   tcase_add_test(tc_dct2_square, test_dct2_MtEty);
   tcase_add_test(tc_dct2_square, test_dct2_EMx);
@@ -311,7 +277,7 @@ Suite *dct2_suite(void)
   */
 
   tc_dct2_pure = tcase_create("dct2_pure");
-  tcase_add_checked_fixture(tc_dct2_pure, setup_pure_square, teardown_pure_square);
+  tcase_add_checked_fixture(tc_dct2_pure, setup_pure_square, teardown);
   tcase_add_test(tc_dct2_pure, test_dct2_MtEt_EMx);
   tcase_add_test(tc_dct2_pure, test_dct2_MtEty);
   tcase_add_test(tc_dct2_pure, test_dct2_EMx);
@@ -326,7 +292,7 @@ Suite *dct2_suite(void)
   */
 
   tc_dct2_tall = tcase_create("dct2_tall");
-  tcase_add_checked_fixture(tc_dct2_tall, setup_tall, teardown_tall);
+  tcase_add_checked_fixture(tc_dct2_tall, setup_tall, teardown);
   tcase_add_test(tc_dct2_tall, test_dct2_MtEt_EMx);
   tcase_add_test(tc_dct2_tall, test_dct2_MtEty);
   tcase_add_test(tc_dct2_tall, test_dct2_EMx);
@@ -340,7 +306,7 @@ Suite *dct2_suite(void)
   /* DCT2 of wide matrix.
   */
   tc_dct2_wide = tcase_create("dct2_wide");
-  tcase_add_checked_fixture(tc_dct2_wide, setup_wide, teardown_wide);
+  tcase_add_checked_fixture(tc_dct2_wide, setup_wide, teardown);
   tcase_add_test(tc_dct2_wide, test_dct2_MtEt_EMx);
   tcase_add_test(tc_dct2_wide, test_dct2_MtEty);
   tcase_add_test(tc_dct2_wide, test_dct2_EMx);
