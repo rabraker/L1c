fd7a0f50d33768d7aa986e35ab3053d42ddce70e
arnold (zeus) <abraker@fastmail.com>
2019-06-12 16:23:08 -0600

    Dont print unless asked. And a bit of cleanup.

--- src/l1qc_dct.c
+++ src/l1qc_dct.c
@@ -41,20 +41,18 @@ int l1qc_dct(int mrow, int mcol, double *x_out, int n, double *b, l1c_int *pix_i
   l1c_AxFuns ax_funs;
 
 
-  /* Allocate memory for x and b, which is aligned to DALIGN.
-     Pointer address from caller probably wont be properly aligned.
-   */
-  printf("mrow = %d, mcol=%d, n=%d\n", mrow, mcol, n);
-
   status = l1c_setup_dct_transforms(n, mrow, mcol, pix_idx, &ax_funs);
 
-  if (status != 0 || !ax_funs.Ax || !ax_funs.Aty || !ax_funs.AtAx){
+  if (status != 0 || !ax_funs.Ax || !ax_funs.Aty || !ax_funs.AtAx || !ax_funs.Mx){
     fprintf(stderr, "Error setup up dct. Exiting.\n");
     status = L1C_DCT_INIT_FAILURE;
     return status;
   }
 
-  double *eta_0 = l1c_malloc_double(mtot);
+  /* Allocate memory for x and b, which is aligned to DALIGN.
+     Pointer address from caller probably wont be properly aligned.
+  */
+  double *eta_0 = l1c_calloc_double(mtot);
   double *b_ours = l1c_malloc_double(n);
 
   if ( !b_ours || !eta_0){
@@ -63,10 +61,7 @@ int l1qc_dct(int mrow, int mcol, double *x_out, int n, double *b, l1c_int *pix_i
     goto exit1;
   }
 
-  /* eta_0 init is only needed for when we use MKL.*/
-  l1c_init_vec(mtot, eta_0, 0);
   cblas_dcopy(n, b, 1, b_ours, 1);
-
   ax_funs.Aty(b_ours, eta_0);
 
   *lb_res = l1c_l1qc_newton(mtot, eta_0, n, b_ours, opts, ax_funs);
@@ -74,23 +69,20 @@ int l1qc_dct(int mrow, int mcol, double *x_out, int n, double *b, l1c_int *pix_i
     status = lb_res->status;
     goto exit2;
   }
+
   /* We solved for eta in the DCT domain. Transform back to
-     standard coorbbdinates.
-  */
-
+     standard coordinates. */
   ax_funs.Mx(eta_0, x_out);
-
   cblas_dcopy(mtot, eta_0, 1, x_out, 1);
 
-
-  tv_end = l1c_get_time();
-
-  double time_total = l1c_get_time_diff(tv_start, tv_end);
-
-  printf("total c time: %f\n", time_total);
-  printf("total-newton-iter: %d\n", lb_res->total_newton_iter);
-  printf("total-cg-iter: %d\n", lb_res->total_cg_iter);
-  printf("time per cg iter: %g\n", time_total / (double) lb_res->total_cg_iter);
+  if (opts.verbose > 0){
+    tv_end = l1c_get_time();
+    double time_total = l1c_get_time_diff(tv_start, tv_end);
+    printf("total c time: %f\n", time_total);
+    printf("total-newton-iter: %d\n", lb_res->total_newton_iter);
+    printf("total-cg-iter: %d\n", lb_res->total_cg_iter);
+    printf("time per cg iter: %g\n", time_total / (double) lb_res->total_cg_iter);
+  }
 
  exit2:
   ax_funs.destroy(); // Should not call this if ax_setup() failed.
